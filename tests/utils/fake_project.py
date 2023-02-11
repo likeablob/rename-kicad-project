f64b781b1cacd2e6d9ff1e97a134e36a8f9be47f
likeablob <46628917+likeablob@users.noreply.github.com>
2023-02-12 01:56:44 +0900

    Add some tests on renaming KiCad 6 project
    - Some refactoring

--- tests/utils/fake_project.py
+++ tests/utils/fake_project.py
@@ -6,13 +6,18 @@ FakeProject = Tuple[Path, List[Path]]
 FAKE_PROJECT_NAME = "temp-project-1"
 
 
-def create_fake_project(tmp_path: Path) -> FakeProject:
+def create_fake_project(tmp_path: Path, use_kicad_pro: bool = False) -> FakeProject:
     project_name = FAKE_PROJECT_NAME
 
     project_dir = tmp_path / (project_name + "-dir")
     project_dir.mkdir()
 
-    exts = (".pro", ".kicad_pcb", ".rules", ".sch")
+    exts = (
+        ".pro" if not use_kicad_pro else ".kicad_pro",
+        ".kicad_pcb",
+        ".rules",
+        ".sch",
+    )
     file_names = [project_name + x for x in exts] + ["fp-lib-table"]
     file_paths = []
     for fn in file_names:
